328a478f006fc9f5ccecfe5d1f51d2ba77b311d2
Otavio Medeiros <otaviorm@gmail.com>
2016-05-22 13:27:34 -0300

    custom nginx image to run rails app

--- ansible/roles/rails-app-setup/files/puma.rb
+++ ansible/roles/rails-app-setup/files/puma.rb
@@ -3,9 +3,7 @@ threads 8, 10
 
 preload_app!
 
-port 3000
-#bind 'tcp://0.0.0.0:3000'
-#bind 'unix:///var/run/app-socket/rails-app.sock'
+bind 'unix:///var/run/app-socket/rails-app.sock'
 
 on_worker_boot do
   ActiveSupport.on_load(:active_record) do
